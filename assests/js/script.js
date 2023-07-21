c6a99f1dfa9845e73a24a1f4cf2a624f67ab019a
RuvindaTharaka <ruvindapeiris23@gmail.com>
2023-07-21 08:59:13 +0530

    bug fix-9

--- assests/js/script.js
+++ assests/js/script.js
@@ -60,6 +60,19 @@ function loadDescMain(step) {
     var descSub2 = document.getElementById("product_des-2");
     var descSub3 = document.getElementById("product_des-3");
     var descSub4 = document.getElementById("product_des-4");
+    var descSub5 = document.getElementById("product_des-5");
+    var descTitle = document.getElementById("produc_des_main_title");
+    var productWrap1 = document.getElementById("product_wrap-1");
+    var productWrap2 = document.getElementById("product_wrap-2");
+    var productWrap3 = document.getElementById("product_wrap-3");
+    var productWrap4 = document.getElementById("product_wrap-4");
+    var productWrap5 = document.getElementById("product_wrap-5");
+
+    var productBox1 = document.getElementById("product_box-1");
+    var productBox2 = document.getElementById("product_box-2");
+    var productBox3 = document.getElementById("product_box-3");
+    var productBox4 = document.getElementById("product_box-4");
+    var productBox5 = document.getElementById("product_box-5");
 
 
     var descMain = "The first and foremost step is to map a draft of the final outcome through the eyes of the client. We believe interior designing is personal, rather a unique concern. Getting to know the client’s vision is very vital in bringing out the spectacle. We then carefully inspect the job site at this stage. Design Square will be very attentive to the nature of the space/business, occupants’ lifestyle and behavior, colour matches and favourations and even tiny details such as pets, vehicles, surrounding when creating and designing concepts. Discussions and more discussions, one thing we at Design Square believe in as one important and continuing activity to the desired magic. Budget discussions is also an important corridor to walk-through carefully at this stage. The goal is to delight  the client without making a hole in the pocket. We finalize which design ideology to go on in this imperative phase.";
@@ -71,7 +84,20 @@ function loadDescMain(step) {
             descSub2.textContent = "";
             descSub3.textContent = "";
             descSub4.textContent = "";
+            descSub5.textContent = "";
+            descTitle.textContent = "Product One";
 
+            productWrap1.classList.toggle("product_wrap_clicked");
+            productWrap2.classList.remove("product_wrap_clicked");
+            productWrap3.classList.remove("product_wrap_clicked");
+            productWrap4.classList.remove("product_wrap_clicked");
+            productWrap5.classList.remove("product_wrap_clicked");
+
+            productBox1.classList.toggle("product_box_clicked");
+            productBox2.classList.remove("product_box_clicked");
+            productBox3.classList.remove("product_box_clicked");
+            productBox4.classList.remove("product_box_clicked");
+            productBox5.classList.remove("product_box_clicked");
             break;
         case 2:
             descMain = "We then design. We draw. We map. We sketch. We do a lot of them, guessing what the client would prefer one over the other. After further discussions, we make any alterations, changes, colour grading, and enhancing the design further to fine-tune the best outcome yet to happen.";
@@ -79,7 +105,20 @@ function loadDescMain(step) {
             descSub1.textContent = "";
             descSub3.textContent = "";
             descSub4.textContent = "";
+            descSub5.textContent = "";
+            descTitle.textContent = "Product Two";
 
+            productWrap1.classList.remove("product_wrap_clicked");
+            productWrap2.classList.toggle("product_wrap_clicked");
+            productWrap3.classList.remove("product_wrap_clicked");
+            productWrap4.classList.remove("product_wrap_clicked");
+            productWrap5.classList.remove("product_wrap_clicked");
+
+            productBox1.classList.remove("product_box_clicked");
+            productBox2.classList.toggle("product_box_clicked");
+            productBox3.classList.remove("product_box_clicked");
+            productBox4.classList.remove("product_box_clicked");
+            productBox5.classList.remove("product_box_clicked");
             break;
         case 3:
             descMain = "The magic converted in to reality in this phase. Once the agreeable design is finalized along with other resources, we focus on getting the real work done.In this phase, the excellence of work will be the rule to follow in order to achieve the craftsmanship to the utmost quality and standards. Compromising is out of the context in delivering what we promise.Just-in-Time JIT) delivery of work will also be one of our biggest considerations at this stage. Our expert team will assist and keep a keen eye until the project is fully completed. We then do the final inspections and go through each and every single detail before the project is handed over to the happy client.";
@@ -87,7 +126,20 @@ function loadDescMain(step) {
             descSub2.textContent = "";
             descSub1.textContent = "";
             descSub4.textContent = "";
+            descSub5.textContent = "";
+            descTitle.textContent = "Product Three";
 
+            productWrap1.classList.remove("product_wrap_clicked");
+            productWrap2.classList.remove("product_wrap_clicked");
+            productWrap3.classList.toggle("product_wrap_clicked");
+            productWrap4.classList.remove("product_wrap_clicked");
+            productWrap5.classList.remove("product_wrap_clicked");
+
+            productBox1.classList.remove("product_box_clicked");
+            productBox2.classList.remove("product_box_clicked");
+            productBox3.classList.toggle("product_box_clicked");
+            productBox4.classList.remove("product_box_clicked");
+            productBox5.classList.remove("product_box_clicked");
             break;
         case 4:
             descMain = "Our concern for our clients’ satisfaction is not over yet. We care about the continuing satisfaction of our clients by providing comprehensive support and services even after the successful completion of the project. From enhancements to alterations and modifications, we are happy to continue the delivery of support and services with no delay.We are unique in terms of our tradition of building trusted and long-lasting corporate relationships with our clients.";
@@ -95,7 +147,41 @@ function loadDescMain(step) {
             descSub2.textContent = "";
             descSub3.textContent = "";
             descSub1.textContent = "";
+            descSub5.textContent = "";
+            descTitle.textContent = "Product Four";
 
+            productWrap1.classList.remove("product_wrap_clicked");
+            productWrap2.classList.remove("product_wrap_clicked");
+            productWrap3.classList.remove("product_wrap_clicked");
+            productWrap4.classList.toggle("product_wrap_clicked");
+            productWrap5.classList.remove("product_wrap_clicked");
+
+            productBox1.classList.remove("product_box_clicked");
+            productBox2.classList.remove("product_box_clicked");
+            productBox3.classList.remove("product_box_clicked");
+            productBox4.classList.toggle("product_box_clicked");
+            productBox5.classList.remove("product_box_clicked");
+            break;
+        case 5:
+            descMain = "2Our concern for our clients’ satisfaction is not over yet. We care about the continuing satisfaction of our clients by providing comprehensive support and services even after the successful completion of the project. From enhancements to alterations and modifications, we are happy to continue the delivery of support and services with no delay.We are unique in terms of our tradition of building trusted and long-lasting corporate relationships with our clients.";
+            descSub5.textContent = descMain;
+            descSub4.textContent = "";
+            descSub3.textContent = "";
+            descSub2.textContent = "";
+            descSub1.textContent = "";
+            descTitle.textContent = "Product Five";
+
+            productWrap1.classList.remove("product_wrap_clicked");
+            productWrap2.classList.remove("product_wrap_clicked");
+            productWrap3.classList.remove("product_wrap_clicked");
+            productWrap4.classList.remove("product_wrap_clicked");
+            productWrap5.classList.toggle("product_wrap_clicked");
+
+            productBox1.classList.remove("product_box_clicked");
+            productBox2.classList.remove("product_box_clicked");
+            productBox3.classList.remove("product_box_clicked");
+            productBox4.classList.remove("product_box_clicked");
+            productBox5.classList.toggle("product_box_clicked");
             break;
         default:
             descMain = "Invalid step";
@@ -115,6 +201,11 @@ function scrollUp() {
 window.addEventListener('scroll', scrollUp)
 
 /*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+    distance: '1px',
+    duration: 3800,
+    reset: true,
+})
 
 sr.reveal(`
         .home_content_div_h1,
